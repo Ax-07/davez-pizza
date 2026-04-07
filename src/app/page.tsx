7bf0b271c8788e1c30fdc2e629926b8278e21ccc
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2026-04-07 18:50:49 +0200

    Refactor le composant Home pour remplacer les sections Specialties et PromoBanner par About, tout en commentant les imports correspondants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 export const dynamic = "force-dynamic";
 
 import { Hero } from "@/components/blocks/hero";
-import { Specialties } from "@/components/blocks/specialties";
-import { PromoBanner } from "@/components/blocks/promo-banner";
+// import { Specialties } from "@/components/blocks/specialties";
+// import { PromoBanner } from "@/components/blocks/promo-banner";
 import { HoursAddress } from "@/components/blocks/hours-address";
 import { Reviews } from "@/components/blocks/reviews";
+import { About } from "@/components/blocks/about";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Specialties />
-      <PromoBanner />
+      <About />
+      {/* <Specialties /> */}
+      {/* <PromoBanner /> */}
       <HoursAddress />
       <Reviews />
     </main>
