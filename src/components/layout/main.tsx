de32c591b1ebd56303ebfa14a4c4b696fe0f5286
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2026-04-08 08:31:20 +0200

    Ajout de la classe overflow-x-hidden au composant Main pour gérer le débordement horizontal

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -1,6 +1,6 @@
 
 export const Main: React.FC<React.ComponentProps<'main'>> = (props) => {
   return (
-    <main className="flex min-h-screen items-start justify-center py-16 bg-background" {...props} />
+    <main className="flex min-h-screen items-start justify-center py-16 bg-background overflow-x-hidden" {...props} />
   );
 }
