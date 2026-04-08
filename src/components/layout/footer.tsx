c99df02e84b9f50a7d2d6e2be81f3bad3a85fe35
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2026-04-08 08:31:11 +0200

    Mise à jour du composant Footer : réorganisation des éléments d'adresse et ajustement des classes CSS

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,8 +7,8 @@ export const Footer = () => {
   return (
     <footer className="border-t bg-background">
       <div className="flex flex-col gap-4 items-center justify-center max-w-6xl mx-auto px-4 py-12">
-        <address className="flex items-center justify-center gap-6">
-          <div className="flex items-start gap-2 text-sm text-muted-foreground mt-8">
+        <address className="flex items-center justify-center lg:gap-6 max-md:flex-col text-center not-italic">
+          <div className="order-2 lg:order-1 flex items-start gap-2 text-sm text-muted-foreground mt-8">
             <MapPin className="size-4 mt-0.5 shrink-0" />
             <span>
               {BUSINESS.address.street}
@@ -16,8 +16,8 @@ export const Footer = () => {
               {BUSINESS.address.postalCode} {BUSINESS.address.city}
             </span>
           </div>
-          <Logo size={"compact"} />
-          <div className="flex items-center gap-2 text-sm text-muted-foreground mt-8">
+          <Logo size={"medium"} className="order-1 lg:order-2"/>
+          <div className="order-3 lg:order-3 flex items-center gap-2 text-sm text-muted-foreground mt-8">
             <Phone className="size-4 shrink-0" />
             <a href={`tel:${BUSINESS.phone.replace(/\s/g, "")}`} className="hover:text-foreground transition-colors">
               {BUSINESS.phone}
